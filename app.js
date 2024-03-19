cd39e31e267e92a02caef0e4d8e15fb632e5ca69
@phuongtd.wiicamp <phuongtd@wiicamp.com>
2024-03-19 20:04:31 +0700

    add node file

---
+++ app.js
@@ -0,0 +1,21 @@
+// 1 Tạo một tệp có tên "app.js" và thêm đoạn mã sau:
+const http = require('http');
+// import http from 'http';
+
+const hostname = '127.0.0.1';
+const port = 4000;
+
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/html');
+  res.end('<strong>Hello Trinh Phuong</strong>');
+});
+
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
+
+// 2. Lưu tệp "app.js".
+// 3. Mở Terminal hoặc Command Prompt và di chuyển đến thư mục chứa tệp "app.js".
+// 4. Chạy lệnh sau để khởi động máy chủ web: node app.js
+// 5. Mở trình duyệt web và truy cập vào địa chỉ "http://localhost:3000".
